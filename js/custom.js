480d51f366614f3514ce3245809c4fb05667f936
Eshun Sharma <eshun247@gmail.com>
2016-02-27 22:00:33 +0530

    Added contact page

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-	 $('#myModal').modal('show');
-})
+$('#go-next').click(function(){
+	$.scrollTo($('.about-eshun'), 1000);
+});
+
+$('#li-about-eshun').click(function(){
+	$.scrollTo($('.about-eshun'), 1000);
+});
+
+$('#li-skills-eshun').click(function(){
+	$.scrollTo($('.skills-eshun'), 1000);
+});
+
+$('#li-contact-eshun').click(function(){
+	$.scrollTo($('.contact-eshun'), 1000);
+});
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.about-eshun');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top - 52) {
+          $('.navbar-eshun').css('background', 'rgba(12, 32, 75, 0.95)');
+       } else {
+          $('.navbar-eshun').css('background-color', 'rgba(12, 32, 75, 0.1)');
+       }
+   });
+});
